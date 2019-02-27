99a7e6f2ef26eac82a44c33c8554f6bb801cbd79
juan <juank7392@gmail.com>
2019-02-27 10:47:45 -0500

    Version con el sys auth creado pero sin modificar migraciones y sin modelos

---
+++ resources/views/home.blade - copia.php
@@ -0,0 +1,18 @@
+@extends('master')
+@section('title','Home')
+@section('content')
+<div class="container">
+    <div class="row banner">
+        <div class="col-md-12">
+            <h1 class="text-center margin-top-100 editContent">
+            Modulo de Tickets    
+            </h1>
+            <h3 class="text-center margin-top-100 editContent">Haz requerimientos y peticiones para los clientes de tu empresa</h3>
+                <div class="text-center">
+                    <img src="http://cuatroparedes.com.co/video/tickets.jpg" width="600" height="350" alt="">
+                </div>
+        </div>
+    </div>
+
+</div>
+@endsection
